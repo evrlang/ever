8b9143a3b59b8196f1073de21bf6305a9125fd52
pouya1dev <291070369+pouya1dev@users.noreply.github.com>
2026-09-19 00:58:13 +0330

    Fixing bugs in evrpkg

--- evrpkg/source/argin.d
+++ evrpkg/source/argin.d
@@ -1,14 +1,14 @@
 module argin;
 
 import std.stdio, std.regex, std.string, std.path, std.json, core.stdc.stdlib, std.file, std.string;
-
+import std.net.curl;
 void arginit(string[] args)
 {
     if (args[1] == "install")
     {
         if (!exists(expandTilde("~/.evrpkg")) || !isDir(expandTilde("~/.evrpkg")))
         {
-            system("mkdir ~/.evrpkg");
+            mkdir(expandTilde("~/.evrpkg"));
         }
         if (!exists(expandTilde("~/.evrpkg/index.json")))
         {
@@ -20,7 +20,10 @@ void arginit(string[] args)
             JSONValue root = parseJSON(readText(expandTilde("~/.evrpkg/index.json")));
             string ver = root["version"].str;
             //writeln("\033[1m\033[32mIndex file version ", ver, " fetched. It will be used as the source for packages.\033[0m");
-            system("wget -q -P ~/.evrpkg/ https://raw.githubusercontent.com/evrlang/evrpkg/refs/heads/main/last.txt");
+            auto vg = get("https://raw.githubusercontent.com/evrlang/evrpkg/refs/heads/main/last.txt");
+            auto gf = File(expandTilde("~/.evrpkg/last.txt"), "w");
+            gf.write(vg);
+            gf.close();
             if(ver != readText(expandTilde("~/.evrpkg/last.txt")).strip()){
                 writeln("Notice: May you need update your Package index with everpkg update?");
             }
@@ -39,24 +42,32 @@ void arginit(string[] args)
                     bool preinstalled = false;
                     if (exists(pkg["pkgname"].str) && isDir(pkg["pkgname"].str))
                     {
-                        system(toStringz("rm -r " ~ pkg["pkgname"].str));
+                        rmdirRecurse(expandTilde(pkg["pkgname"].str));
                         if (exists(expandTilde("~/evrpkg/") ~ pkg["pkgname"].str) && isDir(expandTilde("~/evrpkg/") ~ pkg["pkgname"].str))
                         {
                             preinstalled = true;
-                            system(toStringz("rm -r " ~ expandTilde("~/evrpkg/pkg/") ~ pkg["pkgname"].str));
+                            rmdirRecurse(expandTilde("~/.evrpkg/pkgs/" ~ pkg["pkgname"].str));
                         }
                     }
                     if (pkg["source"].str.startsWith("https://github.com/")) system(toStringz("git clone --depth 1 " ~ pkg["source"].str));
-                    else system(toStringz("wget -q -P ~/.evrpkg/ " ~ pkg["source"].str));
+                    else {
+                        auto lk = get(pkg["source"].str);
+                        std.file.write(pkg["pkgname"].str ~ ".evrpk", lk);
+                    }
                     writeln("\033[32m\033[1mDownload @" ~ pkg["username"].str ~ ":" ~ pkg["pkgname"].str ~ " finished.\033[0m");
                     writeln(pkg["desc"].str);
-                    system(toStringz("mv " ~ pkg["pkgname"].str ~ " ~/.evrpkg/pkgs/"));
+                    mkdirRecurse(expandTilde("~/.evrpkg/pkgs/" ~ pkg["pkgname"].str));
+                    rename(expandTilde(pkg["pkgname"].str), expandTilde("~/.evrpkg/pkgs/" ~ pkg["pkgname"].str));
                     writeln("Install progress finshed, Done.");
-                    if (readText(expandTilde("~/.evrpkg/list.txt")).indexOf(args[2]) == -1)
+                    if (exists(expandTilde("~/.evrpkg/list.txt")) && readText(expandTilde("~/.evrpkg/list.txt")).indexOf(args[2]) == -1)
                     {
-                    auto file = File(expandTilde("~/.evrpkg/list.txt"), "a");
-                    file.write(pkg["pkgname"].str ~ "\n");
-                    file.close();
+                    	auto file = File(expandTilde("~/.evrpkg/list.txt"), "a");
+                    	file.write(pkg["pkgname"].str ~ "\n");
+                    	file.close();
+                    } else {
+                        auto file = File(expandTilde("~/.evrpkg/list.txt"), "w");
+				        file.write(pkg["pkgname"].str ~ "\n");
+				        file.close();
                     }
                 } else {
                     write("\033[31m\033[1mError: " ~ "\033[0m");
@@ -71,20 +82,28 @@ void arginit(string[] args)
         
     } else if (args[1] == "update")
     {
-        system("wget -q -O ~/.evrpkg/index.json https://raw.githubusercontent.com/evrlang/evrpkg/refs/heads/main/index.json");
+        if (!exists(expandTilde("~/.evrpkg")))
+        {
+            mkdir(expandTilde("~/.evrpkg"));
+        }
+        auto vg = get("https://raw.githubusercontent.com/evrlang/evrpkg/refs/heads/main/index.json");
+        auto gf = File(expandTilde("~/.evrpkg/index.json"), "w");
+        gf.write(vg);
+        gf.close();
         JSONValue root = parseJSON(readText(expandTilde("~/.evrpkg/index.json")));
         string ver = root["version"].str;
         writeln("\033[1m\033[32mIndex file version ", ver, " fetched. It will be used as the source for packages.\033[0m");
     } else if (args[1] == "clean"){
-        system(toStringz("rm -r " ~ expandTilde("~/.evrpkg/")));
+        if (exists(expandTilde("~/.evrpkg")) && isDir(expandTilde("~/.evrpkg"))) rmdirRecurse(expandTilde("~/.evrpkg"));
     } else {
         foreach(pkg; readText(expandTilde("~/.evrpkg/list.txt")).splitLines())
         {
-            //writeln(pkg.strip);
-            if (exists(expandTilde("~/.evrpkg/pkgs/") ~ pkg.strip()) && isDir(expandTilde("~/.evrpkg/pkgs/") ~ pkg.strip()))
+            pkg = pkg.strip();
+            if (pkg.strip() == args[1] && exists(expandTilde("~/.evrpkg/pkgs/"  ~ pkg)) && isDir(expandTilde("~/.evrpkg/pkgs/") ~ pkg.strip()))
             {
                 //writeln(expandTilde("~/.evrpkg/" ~ pkg.strip() ~ "/") ~ pkg.strip() ~ ".ever");
                 system(toStringz("ever -ast " ~ expandTilde("~/.evrpkg/pkgs/" ~ pkg.strip() ~ "/") ~ pkg.strip() ~ ".ever"));
+                //writeln("ever -ast " ~ expandTilde("~/.evrpkg/pkgs/" ~ pkg.strip() ~ "/") ~ pkg.strip() ~ ".ever");
             } else {
                 continue;
             }
